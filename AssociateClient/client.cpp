3b5dac8add4a2adc7daed81e755840a8444da519
Jeong <jjb080319@gmail.com>
2022-07-05 14:07:44 +0900

    additem done

--- AssociateClient/client.cpp
+++ AssociateClient/client.cpp
@@ -15,11 +15,21 @@ client::~client()
 
 void client::refresh()
 {
-
+    ui->list->clear();
+    query_string = "SELECT name FROM beach";
+    query.exec(QString::fromStdString(query_string));
+    query.first();
+    while(query.value(0).toString() != "")
+    {
+        if(query.value(0).toString() == "")
+            break;
+        ui->list->addItem(query.value(0).toString());
+        query.next();
+    }
 }
 
 void client::on_refresh_clicked()
 {
-
+    refresh();
 }
 
