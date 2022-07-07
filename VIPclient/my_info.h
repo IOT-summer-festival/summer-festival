f0333542c2d7237b18cff54a60f2bf7cc40e37e2
Jeong <jjb080319@gmail.com>
2022-07-07 16:21:12 +0900

    .

--- VIPclient/my_info.h
+++ VIPclient/my_info.h
@@ -1,46 +1,46 @@
-#ifndef MY_INFO_H
-#define MY_INFO_H
+    #ifndef MY_INFO_H
+    #define MY_INFO_H
 
-#include <QDialog>
-#include <sstream>
-#include <QList>
-#include "change_pw.h"
-#include "client.h"
-#include "hotel_reserv.h"
+    #include <QDialog>
+    #include <sstream>
+    #include <QList>
+    #include "change_pw.h"
+    #include "client.h"
+    #include "hotel_reserv.h"
 
-namespace Ui {
-class my_info;
-}
+    namespace Ui {
+    class my_info;
+    }
 
-class my_info : public QDialog
-{
-    Q_OBJECT
+    class my_info : public QDialog
+    {
+        Q_OBJECT
 
-public:
-    explicit my_info(QString id,QWidget *parent = nullptr);
-    ~my_info();
+    public:
+        explicit my_info(QString id,QWidget *parent = nullptr);
+        ~my_info();
 
-private slots:
-    void on_refresh_clicked();
-    void refresh();
+    private slots:
+        void on_refresh_clicked();
+        void refresh();
 
-    void on_cancel_clicked();
+        void on_cancel_clicked();
 
-    void on_out_clicked();
+        void on_out_clicked();
 
-    void on_change_pw_clicked();
+        void on_change_pw_clicked();
 
-    void on_ch_hotel_clicked();
+        void on_ch_hotel_clicked();
 
-    void on_ch_guide_clicked();
+        void on_ch_guide_clicked();
 
-private:
-    QString guide;
-    QString hotel;
-    QString id;
-    std::string query_string;
-    QSqlQuery query;
-    Ui::my_info *ui;
-};
+    private:
+        QString guide;
+        QString hotel;
+        QString id;
+        std::string query_string;
+        QSqlQuery query;
+        Ui::my_info *ui;
+    };
 
-#endif // MY_INFO_H
+    #endif // MY_INFO_H
